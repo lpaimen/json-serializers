b17b71dee456f9bf1ac4762d058b5c2d08af5d97
Lauri Paimen <lauri@paimen.info>
2020-02-26 10:49:30 +0200

    Move static/ -> docs/

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const estatic = require("serve-static")
 const PORT = 3000
 
 const server = express()
-server.use(estatic("static"))
+server.use(estatic("docs"))
 server.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`)
 })
